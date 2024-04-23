6dae645f1c54dbcced05c05210cd000b5df82fac
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-04-23 11:46:44 +0300

    working on localisation

---
+++ script/localisation/english.js
@@ -0,0 +1,26 @@
+const english = {
+	code: "en",
+	name: "English",
+	// Dialog
+	story_intro_1: "Ronald hasn't seen his grandfather in 20 years and was pretty anxious about the visit.",
+	story_intro_2:
+		"He remembers his grandfather as a sweet guy, always ready to share \n with him life advice and wisdom, that he learned the hard way in life.",
+	story_intro_3: " Ronald knocks at the door, and there was his grandfather, a grin on his face.",
+	story_intro_4: "Hey Ronald, long time",
+	story_intro_5: "Hello",
+	story_intro_6: "They sat face to face in his grandfather's personal study.",
+	story_intro_7: "You wanted to see me... here I am. How are you?",
+	story_intro_8: "Let's skip the pleasantries and all the boring stuff, eh?",
+	story_intro_9: "I'm about to die.",
+	story_intro_10: "Ok.",
+	story_intro_11:
+		"So... because you are practically my only family left, I am\nleaving everything I own, and the money I saved for you, but there is a catch...",
+	story_intro_12: "I am giving it all to you if you complete a few tasks.",
+	story_intro_13: "Which tasks would those be?",
+	story_intro_14: "I am giving you 20000€ for flight tickets. I need you to retrieve 4 items for me from around the world.",
+	story_intro_15: "Accomplish this and you can have everything, which is considerable by the way.",
+	story_intro_16: "If you don't make it, I'm liquidating everything I own, and giving the money to charity, my savings included.",
+	story_intro_17: "Well, I guess I need to pack my things then!",
+	story_intro_18: "*grins* Brave lad! Here's the 20000€ in cash.",
+	story_intro_19: "Good luck!",
+};
---
+++ script/localisation/finnish.js
@@ -0,0 +1,26 @@
+const finnish = {
+	code: "fi",
+	name: "Suomi",
+	// Dialog
+	story_intro_1: "Ronald ei ole nähnyt isoisäänsä 20 vuoteen ja jännitti odotettua tapaamista.",
+	story_intro_2:
+		"Hän muistaa isoisänsä mukavana tyyppinä joka on aina valmiina jakamaan elämän viisauksiaan jotka hän oppi kantapään kautta.",
+	story_intro_3: "Ronald koputtaa isoisänsä ovelle, jonka takana hän odottaa virne naamallaan.",
+	story_intro_4: "Terve Ronaldi, pitkästä aikaa",
+	story_intro_5: "Morjes",
+	story_intro_6: "He siirtyivät isoisän työhuoneeseen ja istuivat vastakkain.",
+	story_intro_7: "Nii, sä halusit nähdä... jote täs mä oon. Miten on menny?",
+	story_intro_8: "Ei nyt ruveta käymään mittää tylsiä elämän juttuja käymää läpi.",
+	story_intro_9: "Kuolema kolkuttaa pian.",
+	story_intro_10: "Vai niin",
+	story_intro_11:
+		"Jotenkas... sä kun satut olemaan käytännössä mun koko perhe, niin oon päättänyt jättää kaiken mitä omistan sulle... yhellä ehdolla.",
+	story_intro_12: "Mä jätän kaiken sulle jos hoidat pari juttua mulle.",
+	story_intro_13: "Mitä sä tarviit?",
+	story_intro_14: "Heitän sulle 20 tonnii, käytä se raha lentämisee. Tartten 4 tavaraa eripuolilt maailmaa.",
+	story_intro_15: "Tässä kun onnistut niin isot rahat ja omaisuudet siirtyy sun nimiin.",
+	story_intro_16: "Jos homma ei luista niin minä pistän kaiken mitä omistan hyväntekeväisyyteen.",
+	story_intro_17: "Kaitpa mun pitää mennä sit pakkaamaan!",
+	story_intro_18: "*virnuilee* Nonih, otappa tästä nää rahat!",
+	story_intro_19: "Ja onnea matkaan!",
+};
