5a0ed2d0b23ab9e6086b28f8273ebf59336e4842
Timppav <tino.vuorela@gmail.com>
2024-04-27 02:19:45 +0300

    Prototype main menu HTML/CSS/JS

---
+++ script/main/mainmenu.js
@@ -0,0 +1,15 @@
+function startGame() {
+  console.log("Start");
+}
+
+function loadGame() {
+  console.log("Load");
+}
+
+function readHelp () {
+  console.log("Help");
+}
+
+function readStory () {
+  console.log("Story");
+}
