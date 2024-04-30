f389485119b1abf720a47f199da20eacb0d0dfbb
Timppav <tino.vuorela@gmail.com>
2024-04-30 14:31:26 +0300

    Help button now opens a help menu

--- script/main/mainmenu.js
+++ script/main/mainmenu.js
@@ -1,21 +1,23 @@
-const gamediv = document.querySelector("#game");
+const gamescreen = document.querySelector("#game");
 const mainmenu = document.querySelector("#mainmenu");
+const helpscreen = document.querySelector("#helpscreen");
+
 const startbutton = document.querySelector("#startGame");
 const loadbutton = document.querySelector("#loadGame");
 const helpbutton = document.querySelector("#readHelp");
 const storybutton = document.querySelector("#readStory");
+const returnbutton = document.querySelector("#return");
 
-startbutton.addEventListener("click", function (evt) {
-	console.log("Start");
-	mainmenu.style.display = "none";
-	gamediv.hidden = false;
+startbutton.addEventListener("click", function () {
+	mainmenu.hidden = true;
+	gamescreen.hidden = false;
 	setup();
 })
 
-helpbutton.addEventListener("click", function(evt) {
-	const newdiv = document.createElement("div");
-	const h1 = document.createElement("h1");
-	newdiv.class = "helpscreen";
-	h1.textContent = "Help";
+helpbutton.addEventListener("click", function() {
+	helpscreen.hidden = false;
+})
 
+returnbutton.addEventListener("click", function() {
+	helpscreen.hidden = true;
 })
