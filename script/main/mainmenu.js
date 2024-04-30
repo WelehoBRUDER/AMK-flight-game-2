2e8f5e702cb0a617e0e26b7dc14a7dba05737e10
Timppav <tino.vuorela@gmail.com>
2024-04-30 10:41:29 +0300

    Fucking around with hidden property in game div

--- script/main/mainmenu.js
+++ script/main/mainmenu.js
@@ -1,13 +1,11 @@
-const mapdiv = document.querySelector("#map");
-const mainmenu = document.querySelector("#mainmenu");
-const buttons = document.querySelector("#buttons");
+const mainmenudiv = document.querySelector("#mainmenu");
+const gamediv = document.querySelector("#game");
 const startbutton = document.querySelector("#startGame");
 const loadbutton = document.querySelector("#loadGame");
 const helpbutton = document.querySelector("#readHelp");
 const storybutton  = document.querySelector("#readStory");
 
 startbutton.addEventListener("click", function(evt) {
-  console.log("Start");
-  mainmenu.style.display = "none";
-  mapdiv.style.display = "block";
+  mainmenudiv.style.display = "none";
+  gamediv.hidden = false;
 });
