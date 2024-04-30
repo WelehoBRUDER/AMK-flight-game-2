27ee55700efc3c215b13909bbe92a893f8f8ace9
Timppav <tino.vuorela@gmail.com>
2024-04-30 10:05:48 +0300

    Added main menu to index.html, and added div hiding functionality to the start button

--- script/main/mainmenu.js
+++ script/main/mainmenu.js
@@ -1,15 +1,13 @@
-function startGame() {
+const mapdiv = document.querySelector("#map");
+const mainmenu = document.querySelector("#mainmenu");
+const buttons = document.querySelector("#buttons");
+const startbutton = document.querySelector("#startGame");
+const loadbutton = document.querySelector("#loadGame");
+const helpbutton = document.querySelector("#readHelp");
+const storybutton  = document.querySelector("#readStory");
+
+startbutton.addEventListener("click", function(evt) {
   console.log("Start");
-}
-
-function loadGame() {
-  console.log("Load");
-}
-
-function readHelp () {
-  console.log("Help");
-}
-
-function readStory () {
-  console.log("Story");
-}
+  mainmenu.style.display = "none";
+  mapdiv.style.display = "block";
+});
