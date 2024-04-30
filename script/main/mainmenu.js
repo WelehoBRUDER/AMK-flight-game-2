9f6f6888d136d075f921ac96edf5e86ce9a6ad3b
Timppav <tino.vuorela@gmail.com>
2024-04-30 10:42:29 +0300

    Revert "Fucking around with hidden property in game div"
    This reverts commit 2e8f5e702cb0a617e0e26b7dc14a7dba05737e10.

--- script/main/mainmenu.js
+++ script/main/mainmenu.js
@@ -1,11 +1,13 @@
-const mainmenudiv = document.querySelector("#mainmenu");
-const gamediv = document.querySelector("#game");
+const mapdiv = document.querySelector("#map");
+const mainmenu = document.querySelector("#mainmenu");
+const buttons = document.querySelector("#buttons");
 const startbutton = document.querySelector("#startGame");
 const loadbutton = document.querySelector("#loadGame");
 const helpbutton = document.querySelector("#readHelp");
 const storybutton  = document.querySelector("#readStory");
 
 startbutton.addEventListener("click", function(evt) {
-  mainmenudiv.style.display = "none";
-  gamediv.hidden = false;
+  console.log("Start");
+  mainmenu.style.display = "none";
+  mapdiv.style.display = "block";
 });
