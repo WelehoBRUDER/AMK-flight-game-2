41d02586318dfc1e5ae1c9ef7b95b9a717dbef01
Timppav <tino.vuorela@gmail.com>
2024-05-01 16:45:12 +0300

    Restructured the interface a bunch

--- script/main/mainmenu.js
+++ script/main/mainmenu.js
@@ -1,23 +1,44 @@
-const gamescreen = document.querySelector("#game");
+const maparea = document.querySelector("#map-area");
+const mapscreen = document.querySelector("#map");
+const dialogbox = document.querySelector("#dialog-box");
+
 const mainmenu = document.querySelector("#mainmenu");
 const helpscreen = document.querySelector("#helpscreen");
 
-const startbutton = document.querySelector("#startGame");
-const loadbutton = document.querySelector("#loadGame");
-const helpbutton = document.querySelector("#readHelp");
-const storybutton = document.querySelector("#readStory");
+const startbutton = document.querySelector("#startgame");
+const loadbutton = document.querySelector("#loadgame");
+const helpbutton = document.querySelector("#readhelp");
+const storybutton = document.querySelector("#readstory");
 const returnbutton = document.querySelector("#return");
+const closebutton = document.querySelector("#close-dialog");
+
 
 startbutton.addEventListener("click", function () {
 	mainmenu.hidden = true;
-	gamescreen.hidden = false;
+	helpscreen.hidden = true;
+	maparea.hidden = false;
+	mapscreen.hidden = false;
 	setup();
 })
 
 helpbutton.addEventListener("click", function() {
+	mainmenu.hidden = true;
+	mapscreen.hidden = true;
 	helpscreen.hidden = false;
 })
 
 returnbutton.addEventListener("click", function() {
 	helpscreen.hidden = true;
+	mainmenu.hidden = false;
+	mapscreen.hidden = false;
+})
+
+storybutton.addEventListener("click", function() {
+	dialogbox.style.display = "flex";
+	closebutton.hidden = false;
+})
+
+closebutton.addEventListener("click", function() {
+	dialogbox.style.display = "none";
+	closebutton.hidden = true;
 })
