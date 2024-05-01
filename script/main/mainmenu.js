a5683def1e0d1138eb893bf504e9eb38144b85df
Timppav <tino.vuorela@gmail.com>
2024-05-02 01:51:16 +0300

    Added sound effects to buttons and a brand-new music toggle button

--- script/main/mainmenu.js
+++ script/main/mainmenu.js
@@ -9,11 +9,30 @@ const startbutton = document.querySelector("#startgame");
 const loadbutton = document.querySelector("#loadgame");
 const helpbutton = document.querySelector("#readhelp");
 const storybutton = document.querySelector("#readstory");
+const musicbutton = document.querySelector("#music");
 const returnbutton = document.querySelector("#return");
 const closebutton = document.querySelector("#close-dialog");
 
+const click = new Audio("sounds/click.mp3");
+const shanty = new Audio("sounds/seashanty2.mp3");
+click.load();
+shanty.load();
+
+let audioplaying = false;
+
+function playAudio(src, volume) {
+	src.volume = volume;
+	src.play();
+}
+
+function playMusic(src, volume) {
+	src.volume = volume;
+	src.loop = true;
+	src.play();
+}
 
 startbutton.addEventListener("click", function () {
+	playAudio(click, 0.3);
 	mainmenu.hidden = true;
 	helpscreen.hidden = true;
 	maparea.hidden = false;
@@ -22,23 +41,38 @@ startbutton.addEventListener("click", function () {
 })
 
 helpbutton.addEventListener("click", function() {
+	playAudio(click, 0.3);
 	mainmenu.hidden = true;
 	mapscreen.hidden = true;
 	helpscreen.hidden = false;
 })
 
 returnbutton.addEventListener("click", function() {
+	playAudio(click, 0.3);
 	helpscreen.hidden = true;
 	mainmenu.hidden = false;
 	mapscreen.hidden = false;
 })
 
 storybutton.addEventListener("click", function() {
+	playAudio(click, 0.3);
 	dialogbox.style.display = "flex";
 	closebutton.hidden = false;
 })
 
 closebutton.addEventListener("click", function() {
+	playAudio(click, 0.3);
 	dialogbox.style.display = "none";
 	closebutton.hidden = true;
+})
+
+musicbutton.addEventListener("click", function() {
+	playAudio(click, 0.3);
+	if (audioplaying) {
+		playMusic(shanty, 0);
+		audioplaying = false;
+	} else {
+		playMusic(shanty, 0.2);
+		audioplaying = true;
+	}
 })
