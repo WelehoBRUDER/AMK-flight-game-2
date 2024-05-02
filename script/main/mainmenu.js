eddd82793b8a549e4f7c7f0e3bacbd02022c0cf4
Timppav <tino.vuorela@gmail.com>
2024-05-02 14:04:11 +0300

    Slight improvements to the options menu

--- script/main/mainmenu.js
+++ script/main/mainmenu.js
@@ -1,9 +1,13 @@
 const maparea = document.querySelector("#map-area");
 const mapscreen = document.querySelector("#map");
 
-const optionbutton = document.querySelector("#option-button");
 const optionsmenu = document.querySelector("#option-menu");
+const startscreen = document.querySelector("#start-screen");
+const loadscreen = document.querySelector("#load-screen");
+const helpscreen = document.querySelector("#help-screen");
+const storyscreen = document.querySelector("#story-screen");
 
+const optionbutton = document.querySelector("#option-button");
 const newgamebutton = document.querySelector("#new-game");
 const loadbutton = document.querySelector("#load-game");
 const helpbutton = document.querySelector("#read-help");
@@ -13,10 +17,7 @@ const closebutton = document.querySelector("#close");
 const closedialog = document.querySelector("#close-dialog");
 const startbutton = document.querySelector("#start-game");
 
-const startscreen = document.querySelector("#start-screen");
-const loadscreen = document.querySelector("#load-screen");
-const helpscreen = document.querySelector("#help-screen");
-const storyscreen = document.querySelector("#story-screen");
+const button = document.querySelector("button");
 
 let optionsopen = false;
 
@@ -27,8 +28,13 @@ function hideBoxes() {
 	storyscreen.hidden = true;
 }
 
+document.querySelectorAll("button").forEach((btn) => {
+    btn.addEventListener("click", () => {
+			soundController.playSound("click", true)
+		});
+});
+
 optionbutton.addEventListener("click", function() {
-	soundController.playSound("click");
 	if (optionsopen) {
 		optionsmenu.style.display = "none";
 		optionsopen = false;
@@ -39,46 +45,39 @@ optionbutton.addEventListener("click", function() {
 })
 
 newgamebutton.addEventListener("click", function() {
-	soundController.playSound("click");
 	hideBoxes();
 	startscreen.hidden = false;
 })
 
 startbutton.addEventListener("click", function() {
-	soundController.playSound("click");
+	optionsmenu.style.display = "none";
 	mapscreen.hidden = false;
 	maparea.hidden = false;
 	setup();
 })
 
 loadbutton.addEventListener("click", function() {
-	soundController.playSound("click");
 	hideBoxes();
 	loadscreen.hidden = false;
 })
 
 helpbutton.addEventListener("click", function() {
-	soundController.playSound("click");
 	hideBoxes();
 	helpscreen.hidden = false;
 })
 
 storybutton.addEventListener("click", function() {
-	soundController.playSound("click");
 	hideBoxes();
 	storyscreen.hidden = false;
 })
 
 musicbutton.addEventListener("click", function() {
-	soundController.playSound("click");
 })
 
 closebutton.addEventListener("click", function() {
-	soundController.playSound("click");
 	optionsmenu.style.display = "none";
 	optionsopen = false;
 })
 
 closedialog.addEventListener("click", function() {
-	soundController.playSound("click");
 })
