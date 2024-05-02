02ee1108624501be3e5ffc1bf7390320c7d4a05c
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-05-02 10:14:17 +0300

    flying works!!!

--- script/main/map.js
+++ script/main/map.js
@@ -14,11 +14,14 @@ async function addMarkers() {
 	// const ports = await routes.getAllNonSmallAirports();
 	for (const port of ports) {
 		const marker = L.marker([port.airport.latitude_deg, port.airport.longitude_deg], { icon: mapIcons.port, interactive: true }).addTo(map);
+		marker.distance = port.distance;
 		const tooltip = new Tooltip({ marker, text: port.airport.name });
 		tooltip.create();
 
 		marker.on("click", (e) => {
-			moveMap(e.latlng.lat, e.latlng.lng);
+			if (map.dragging._enabled) {
+				moveMap(e.latlng.lat, e.latlng.lng, marker.distance);
+			}
 		});
 	}
 }
@@ -28,23 +31,55 @@ function setup() {
 	addMarkers();
 }
 
-async function moveMap(lat2, lon2) {
-	plane.classList.remove("hide");
+function lockMap() {
+	map.touchZoom.disable();
+	map.doubleClickZoom.disable();
+	map.scrollWheelZoom.disable();
+	map.boxZoom.disable();
+	map.keyboard.disable();
+	map.dragging.disable();
+	document.querySelector(".leaflet-control-zoom").style.display = "none";
+}
+
+function unlockMap() {
+	map.touchZoom.enable();
+	map.doubleClickZoom.enable();
+	map.scrollWheelZoom.enable();
+	map.boxZoom.enable();
+	map.keyboard.enable();
+	map.dragging.enable();
+	document.querySelector(".leaflet-control-zoom").style.display = "block";
+}
+
+async function moveMap(lat2, lon2, dist) {
 	const { lat, lon } = currentCords;
+	map.setView([lat, lon], 8, {
+		animate: false,
+	});
+	lockMap();
+	plane.classList.remove("hide");
+
+	const duration = dist * 2;
 	const bearing = await routes.bearing(lat, lon, lat2, lon2);
 	plane.style.setProperty("--angle", `${bearing}deg`);
-	map.setView([lat, lon], map.getZoom());
-	map.setView([lat2, lon2], map.getZoom(), {
-		animate: true,
-		pan: {
-			duration: 2.5,
-		},
-	});
+	//map.setView([lat, lon], 8);
+	console.log(lat, lon, lat2, lon2);
+	console.log(dist);
+	setTimeout(() => {
+		map.setView([lat2, lon2], map.getZoom(), {
+			animate: true,
+			pan: {
+				duration: duration / 1000,
+			},
+		});
+	}, 100);
+
 	setTimeout(() => {
 		plane.classList.add("hide");
 		currentCords.lat = lat2;
 		currentCords.lon = lon2;
-	}, 2700);
+		unlockMap();
+	}, duration + 200);
 }
 
 const mapIcons = {
