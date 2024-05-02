8ccce50b3daec4f56a44b91a80266b6e229e28c6
Timppav <tino.vuorela@gmail.com>
2024-05-02 13:39:20 +0300

    Menu option buttons now open the corresponding box

--- script/main/mainmenu.js
+++ script/main/mainmenu.js
@@ -1,20 +1,31 @@
 const maparea = document.querySelector("#map-area");
 const mapscreen = document.querySelector("#map");
-const dialogbox = document.querySelector("#dialog-box");
 
 const optionbutton = document.querySelector("#option-button");
 const optionsmenu = document.querySelector("#option-menu");
 
-const startbutton = document.querySelector("#startgame");
-const loadbutton = document.querySelector("#loadgame");
-const helpbutton = document.querySelector("#readhelp");
-const storybutton = document.querySelector("#readstory");
+const newgamebutton = document.querySelector("#new-game");
+const loadbutton = document.querySelector("#load-game");
+const helpbutton = document.querySelector("#read-help");
+const storybutton = document.querySelector("#read-story");
 const musicbutton = document.querySelector("#music");
 const closebutton = document.querySelector("#close");
-
 const closedialog = document.querySelector("#close-dialog");
+const startbutton = document.querySelector("#start-game");
+
+const startscreen = document.querySelector("#start-screen");
+const loadscreen = document.querySelector("#load-screen");
+const helpscreen = document.querySelector("#help-screen");
+const storyscreen = document.querySelector("#story-screen");
 
 let optionsopen = false;
+
+function hideBoxes() {
+	startscreen.hidden = true;
+	loadscreen.hidden = true;
+	helpscreen.hidden = true;
+	storyscreen.hidden = true;
+}
 
 optionbutton.addEventListener("click", function() {
 	soundController.playSound("click");
@@ -27,6 +38,12 @@ optionbutton.addEventListener("click", function() {
 	}
 })
 
+newgamebutton.addEventListener("click", function() {
+	soundController.playSound("click");
+	hideBoxes();
+	startscreen.hidden = false;
+})
+
 startbutton.addEventListener("click", function() {
 	soundController.playSound("click");
 	mapscreen.hidden = false;
@@ -36,14 +53,20 @@ startbutton.addEventListener("click", function() {
 
 loadbutton.addEventListener("click", function() {
 	soundController.playSound("click");
+	hideBoxes();
+	loadscreen.hidden = false;
 })
 
 helpbutton.addEventListener("click", function() {
 	soundController.playSound("click");
+	hideBoxes();
+	helpscreen.hidden = false;
 })
 
 storybutton.addEventListener("click", function() {
 	soundController.playSound("click");
+	hideBoxes();
+	storyscreen.hidden = false;
 })
 
 musicbutton.addEventListener("click", function() {
