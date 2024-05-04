0ce486eafa59de3c52bbc25a4ca199281904d90a
Timppav <tino.vuorela@gmail.com>
2024-05-04 03:28:38 +0300

    Added a settings menu, cleaned up variable/id names and commented some code

--- script/main/mainmenu.js
+++ script/main/mainmenu.js
@@ -1,33 +1,46 @@
-const startmenu = document.querySelector("#start-menu");
-const optionsmenu = document.querySelector("#option-menu");
+const start_menu = document.querySelector("#start-menu");
+
+const options_button = document.querySelector("#options-button");
+const options_menu = document.querySelector("#options-menu");
 const buttons = document.querySelector("#buttons");
-const startscreen = document.querySelector("#start-screen");
-const loadscreen = document.querySelector("#load-screen");
-const helpscreen = document.querySelector("#help-screen");
-const storyscreen = document.querySelector("#story-screen");
 
-const maparea = document.querySelector("#map-area");
-const mapscreen = document.querySelector("#map");
+const new_game_button = document.querySelector("#new-game");
+const new_game_screen = document.querySelector("#new-game-screen");
+const start_button = document.querySelector("#start-game");
 
-const optionbutton = document.querySelector("#option-button");
-const newgamebutton = document.querySelector("#new-game");
-const startbutton = document.querySelector("#start-game");
-const loadbutton = document.querySelector("#load-game");
-const helpbutton = document.querySelector("#read-help");
-const storybutton = document.querySelector("#read-story");
-const closedialog = document.querySelector("#close-dialog");
-const musicbutton = document.querySelector("#music");
-const closebutton = document.querySelector("#close");
+const save_load_button = document.querySelector("#save-load");
+const save_load_screen = document.querySelector("#save-load-screen");
 
-let optionsopen = true;
+const help_button = document.querySelector("#read-help");
+const help_screen = document.querySelector("#help-screen");
 
+const story_button = document.querySelector("#read-story");
+const story_screen = document.querySelector("#story-screen");
+const close_dialog = document.querySelector("#close-dialog");
+
+const settings_button = document.querySelector("#settings");
+const settings_screen = document.querySelector("#settings-screen");
+const music_button = document.querySelector("#music");
+const english_button = document.querySelector("#lang-english");
+const finnish_button = document.querySelector("#lang-suomi");
+
+const close_button = document.querySelector("#close");
+
+const map_area = document.querySelector("#map-area");
+const map_screen = document.querySelector("#map");
+
+let options_open = true;
+
+// Hides all the option menu screens. Used right before opening a screen, to prevent overlap.
 function hideBoxes() {
-	startscreen.hidden = true;
-	loadscreen.hidden = true;
-	helpscreen.hidden = true;
-	storyscreen.hidden = true;
+	new_game_screen.hidden = true;
+	save_load_screen.hidden = true;
+	help_screen.hidden = true;
+	story_screen.hidden = true;
+	settings_screen.hidden = true;
 }
 
+// Plays a 'click' sound effect every time any button is clicked.
 document.querySelectorAll("button").forEach((btn) => {
 	btn.addEventListener("click", () => {
 		soundController.playSound("click", true);
@@ -35,81 +48,96 @@ document.querySelectorAll("button").forEach((btn) => {
 });
 
 function hideStart() {
-	startmenu.hidden = true;
+	start_menu.hidden = true;
 }
 
-startmenu.addEventListener("click", function () {
-	startmenu.style.opacity = "0";
-	closebutton.style.display = "none";
+// Fading animation for the start screen
+start_menu.addEventListener("click", function () {
+	start_menu.style.opacity = "0";
+	close_button.style.display = "none";
 	setTimeout(hideStart, 1000);
 });
 
-optionbutton.addEventListener("click", function () {
-	if (optionsopen) {
-		optionsmenu.style.display = "none";
-		optionsopen = false;
+options_button.addEventListener("click", function () {
+	if (options_open) {
+		options_menu.style.display = "none";
+		options_open = false;
 	} else {
-		optionsmenu.style.display = "flex";
-		optionsopen = true;
+		options_menu.style.display = "flex";
+		options_open = true;
 	}
 });
 
-newgamebutton.addEventListener("click", function () {
+new_game_button.addEventListener("click", function () {
 	hideBoxes();
-	startscreen.hidden = false;
+	new_game_screen.hidden = false;
 });
 
-startbutton.addEventListener("click", function () {
-	optionsmenu.style.display = "none";
-	mapscreen.hidden = false;
-	maparea.hidden = false;
-	closebutton.style.display = "block";
-	optionbutton.hidden = false;
+start_button.addEventListener("click", function () {
+	options_menu.style.display = "none";
+	map_screen.hidden = false;
+	map_area.hidden = false;
+	close_button.style.display = "block";
+	options_button.hidden = false;
 	setup();
 });
 
-loadbutton.addEventListener("click", function () {
+save_load_button.addEventListener("click", function () {
 	hideBoxes();
-	loadscreen.hidden = false;
+	save_load_screen.hidden = false;
 });
 
-helpbutton.addEventListener("click", function () {
+help_button.addEventListener("click", function () {
 	hideBoxes();
-	helpscreen.hidden = false;
+	help_screen.hidden = false;
 });
 
-storybutton.addEventListener("click", function () {
+story_button.addEventListener("click", function () {
 	hideBoxes();
 	buttons.hidden = true;
-	storyscreen.hidden = false;
+	story_screen.hidden = false;
 });
 
-closedialog.addEventListener("click", function () {
+close_dialog.addEventListener("click", function () {
 	buttons.hidden = false;
-	storyscreen.hidden = true;
+	story_screen.hidden = true;
 });
 
-musicbutton.addEventListener("click", function () {
+settings_button.addEventListener("click", function() {
+	hideBoxes();
+	settings_screen.hidden = false;
+})
+
+// Pauses the music if it's currently playing, and vice versa. Also toggles the button text.
+music_button.addEventListener("click", function () {
 	if (soundController.isMusicPlaying()) {
 		soundController.pauseMusic();
-		musicbutton.textContent = "Music: OFF";
+		music_button.textContent = "Music: OFF";
 	} else {
 		soundController.playSound("shanty");
-		musicbutton.textContent = "Music: ON";
+		music_button.textContent = "Music: ON";
 	}
 });
 
-closebutton.addEventListener("click", function () {
-	optionsmenu.style.display = "none";
-	optionsopen = false;
+english_button.addEventListener("click", function() {
+
+})
+
+finnish_button.addEventListener("click", function() {
+
+})
+
+close_button.addEventListener("click", function () {
+	options_menu.style.display = "none";
+	options_open = false;
 });
 
 if (dev.enabled) {
 	hideStart();
-	optionsmenu.style.display = "none";
-	mapscreen.hidden = false;
-	maparea.hidden = false;
-	closebutton.style.display = "block";
-	optionbutton.hidden = false;
+	options_menu.style.display = "none";
+	map_screen.hidden = false;
+	map_area.hidden = false;
+	close_button.style.display = "block";
+	options_button.hidden = false;
 	setup();
 }
