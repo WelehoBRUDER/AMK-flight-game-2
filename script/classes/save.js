abba394c89fead25c14e74f61ce7e8ef534dc480
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-05-04 20:01:33 +0300

    working on saves

--- script/classes/save.js
+++ script/classes/save.js
@@ -1,6 +1,7 @@
 class GameState {
 	constructor() {
 		this.savedGames = [];
+		this.currentSave = "";
 	}
 
 	loadSavedGames() {
@@ -10,12 +11,58 @@ class GameState {
 		}
 	}
 
-	saveGame() {}
+	getFile(id) {
+		return this.savedGames.findIndex((save) => save.id === id);
+	}
+
+	sortSaves() {
+		this.savedGames = this.savedGames.sort((a, b) => b.lastSaved.getTime() - a.lastSaved.getTime());
+	}
+
+	// WIP
+	loadGame(id, confirm = false) {
+		const save = this.savedGames[this.getFile(id)];
+		if (save) {
+			try {
+			} catch (err) {
+				console.error("Oopsie", err);
+			}
+		}
+	}
+
+	saveGame(name, id = null, file = null, auto = false) {
+		const saveFile = file ? new SaveFile(file) : new SaveFile({ id, name });
+		this.currentSave = saveFile.id;
+		const existingSaveIndex = this.getFile(id);
+		if (existingSaveIndex > -1) {
+			this.savedGames[existingSaveIndex] = saveFile;
+		} else {
+			this.savedGames.push(saveFile);
+		}
+		this.sortSaves();
+		localStorage.setItem("grandpas-lost-sauce_save_games", JSON.stringify(this.savedGames));
+	}
+
+	saveOver(id, auto) {
+		const save = this.savedGames[this.getFile(id)];
+		if (save) {
+			this.saveGame(save.name, id, save);
+		}
+	}
+
+	deleteSave(id) {
+		const save = this.savedGames[this.getFile(id)];
+		if (save) {
+			this.savedGames = this.savedGames.filter((save) => save.id !== id);
+			localStorage.setItem("grandpas-lost-sauce_save_games", JSON.stringify(this.savedGames));
+		}
+	}
 }
 
 class SaveFile {
 	constructor(file) {
 		this.id = file.id ?? generateID();
+		this.name = file.name;
 		this.game = file.game;
 		this.firstCreated = file.firstCreated ?? new Date();
 		this.lastSaved = new Date();
