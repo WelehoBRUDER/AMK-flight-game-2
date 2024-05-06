545fa413789e488bd6270d057bcf827969114054
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2024-05-06 20:37:42 +0300

    added function for creating elements

--- script/game/ahmeds_dice_game.js
+++ script/game/ahmeds_dice_game.js
@@ -1,217 +1,235 @@
+function createDiceGameElem() {
+
 // Main container
-const mainCon = document.createElement("div");
-mainCon.className = "diceGame";
-document.body.appendChild(mainCon);
+  const mainCon = document.createElement('div');
+  mainCon.className = 'diceGame';
+  document.body.appendChild(mainCon);
 
 // Game title
-const header = document.createElement("h2");
-header.classList.add("game-title");
-header.textContent = "Ahmed's dice duel";
-mainCon.appendChild(header);
+  const header = document.createElement('h2');
+  header.classList.add('game-title');
+  header.textContent = 'Ahmed\'s dice duel';
+  mainCon.appendChild(header);
 
 // NPC Container
-const npcBody = document.createElement("div");
-npcBody.classList.add("npc", "top-left");
-mainCon.appendChild(npcBody);
+  const npcBody = document.createElement('div');
+  npcBody.classList.add('npc', 'body-left');
+  document.body.appendChild(npcBody);
 
 // P Element for displaying Computer's score
-let pElemOne = document.createElement("p");
-pElemOne.classList.add("comp-score");
-pElemOne.textContent = "Ahmed's roll: ";
-npcBody.appendChild(pElemOne);
+  let pElemOne = document.createElement('p');
+  pElemOne.classList.add('comp-score');
+  pElemOne.textContent = 'Ahmed\'s roll: ';
+  npcBody.appendChild(pElemOne);
 
 // Container for the Dice set
-const dices = document.createElement("div");
-dices.className = "diceSet";
-mainCon.appendChild(dices);
+  const dices = document.createElement('div');
+  dices.classList.add('diceSet');
+  mainCon.appendChild(dices);
 
 // PLayer container
-const playerCon = document.createElement("div");
-playerCon.classList.add("player", "bottom-right");
-mainCon.appendChild(playerCon);
+  const playerCon = document.createElement('div');
+  playerCon.classList.add('player', 'body-right');
+  document.body.appendChild(playerCon);
 
 // P Element for displaying Player's score
-let pElemTwo = document.createElement("p");
-pElemTwo.classList.add("player-score");
-pElemTwo.textContent = "Player's roll: ";
-playerCon.appendChild(pElemTwo);
+  let pElemTwo = document.createElement('p');
+  pElemTwo.classList.add('player-score');
+  pElemTwo.textContent = 'Player\'s roll: ';
+  playerCon.appendChild(pElemTwo);
 
 // Roll button for the player
-const rollBtn = document.createElement("button");
-rollBtn.textContent = "Roll the Dices";
-rollBtn.className = "roll";
-mainCon.appendChild(rollBtn);
+  const rollBtn = document.createElement('button');
+  rollBtn.textContent = 'Roll the Dices';
+  rollBtn.className = 'roll';
+  mainCon.appendChild(rollBtn);
 
 // Heading element that shows the win/loss messages
-const endScreenMsg = document.createElement("h2");
-endScreenMsg.classList.add("end-screen", "hidden");
-mainCon.appendChild(endScreenMsg);
+  const endScreenMsg = document.createElement('h2');
+  endScreenMsg.classList.add('end-screen', 'hidden');
+  mainCon.appendChild(endScreenMsg);
+
+  // Element for displaying the remaining rolls
+  let h3Elem = document.createElement('h3');
+  mainCon.appendChild(h3Elem);
+
+  return {
+    mainCon,
+    header,
+    npcBody,
+    pElemOne,
+    dices,
+    playerCon,
+    pElemTwo,
+    rollBtn,
+    h3Elem,
+    endScreenMsg,
+  };
+}
+
+const gameElem = createDiceGameElem();
 
 function diceCreation() {
-	const diceClass = document.createElement("div");
-	diceClass.className = "dice";
+  const diceClass = document.createElement('div');
+  diceClass.className = 'dice';
 
-	const faceClasses = ["front", "back", "top", "bottom", "right", "left"];
+  const faceClasses = ['front', 'back', 'top', 'bottom', 'right', 'left'];
 
-	for (let i = 0; i < faceClasses.length; i++) {
-		const face = document.createElement("div");
-		face.className = `face ${faceClasses[i]}`;
-		diceClass.appendChild(face);
-	}
-	return diceClass;
+  for (let i = 0; i < faceClasses.length; i++) {
+    const face = document.createElement('div');
+    face.className = `face ${faceClasses[i]}`;
+    diceClass.appendChild(face);
+  }
+  return diceClass;
 }
 
 let computerTotal = 0;
 
-function rollComputerDices() {
-	const diceSet = mainCon.querySelector(".diceSet");
-	diceSet.innerHTML = "";
+function rollComputerDices(gameElem) {
+  console.log(gameElem.mainCon);
+  const diceSet = gameElem.mainCon.querySelector('.diceSet');
+  diceSet.innerHTML = '';
 
-	rollBtn.disabled = true;
-	setTimeout(() => {
-		rollBtn.disabled = false;
-	}, 3500 / settings.animationSpeed);
+  gameElem.rollBtn.disabled = true;
+  setTimeout(() => {
+    gameElem.rollBtn.disabled = false;
+  }, 3500);
 
-	const compDiceValues = [];
+  const compDiceValues = [];
 
-	// Create and append three separate dice models for the comp
-	for (let i = 0; i < 3; i++) {
-		const compDice = diceCreation();
-		diceSet.appendChild(compDice);
+  // Create and append three separate dice models for the comp
+  for (let i = 0; i < 3; i++) {
+    const compDice = diceCreation();
+    diceSet.appendChild(compDice);
 
-		const value = Math.floor(Math.random() * 6) + 1;
-		compDiceValues.push(value);
+    const value = Math.floor(Math.random() * 6) + 1;
+    compDiceValues.push(value);
 
-		console.log("Comp dice values: ", compDiceValues);
+    console.log('Comp dice values: ', compDiceValues);
 
-		displayDiceValues(compDice, [value]);
-	}
-	for (let i = 0; i < compDiceValues.length; i++) {
-		computerTotal += compDiceValues[i];
-	}
-	setTimeout(() => {
-		pElemOne.innerHTML = `Ahmed's roll: <b style="color: darkred">${computerTotal}</b>`;
-	}, 3500 / settings.animationSpeed);
-	updateRollsLeft();
+    displayDiceValues(compDice, [value]);
+  }
+  for (let i = 0; i < compDiceValues.length; i++) {
+    computerTotal += compDiceValues[i];
+  }
+  setTimeout(() => {
+    gameElem.pElemOne.innerHTML = `Ahmed's roll: <b style="color: darkred">${computerTotal}</b>`;
+  }, 3500);
+  updateRollsLeft(gameElem);
 }
 
-function updateRollsLeft() {
-	if (attempts >= 2) {
-		h1Elem.innerHTML = `Rolls left: <b style="color: green">${attempts}</b>`;
-	} else {
-		h1Elem.innerHTML = `Rolls left: <b style="color: red">${attempts}</b>`;
-	}
+function updateRollsLeft(gameElem) {
+  if (attempts >= 2) {
+    gameElem.h3Elem.innerHTML = `Rolls left: <b style="color: green">${attempts}</b>`;
+  } else {
+    gameElem.h3Elem.innerHTML = `Rolls left: <b style="color: red">${attempts}</b>`;
+  }
 }
-
-let h1Elem = document.createElement("h1");
-mainCon.appendChild(h1Elem);
 
 let attempts = 3;
 let playerTotal = 0;
 
-function rollPlayerDices() {
-	if (attempts > 0) {
-		attempts--;
+function rollPlayerDices(gameElem) {
+  if (attempts > 0) {
+    attempts--;
 
-		const diceSet = mainCon.querySelector(".diceSet");
-		diceSet.innerHTML = "";
+    const diceSet = gameElem.mainCon.querySelector('.diceSet');
+    diceSet.innerHTML = '';
 
-		const playerDiceValues = [];
+    const playerDiceValues = [];
 
-		// Create and append three separate dice models for the player
-		for (let i = 0; i < 3; i++) {
-			const playerDice = diceCreation();
-			diceSet.appendChild(playerDice);
+    // Create and append three separate dice models for the player
+    for (let i = 0; i < 3; i++) {
+      const playerDice = diceCreation();
+      diceSet.appendChild(playerDice);
 
-			const value = Math.floor(Math.random() * 6) + 1;
-			playerDiceValues.push(value);
+      const value = Math.floor(Math.random() * 6) + 1;
+      playerDiceValues.push(value);
 
-			console.log("Player dice values: " + playerDiceValues);
+      console.log('Player dice values: ' + playerDiceValues);
 
-			displayDiceValues(playerDice, [value]);
-		}
-		rollBtn.disabled = true;
-		setTimeout(() => {
-			rollBtn.disabled = false;
-		}, 3500 / settings.animationSpeed);
+      displayDiceValues(playerDice, [value]);
+    }
+    gameElem.rollBtn.disabled = true;
+    setTimeout(() => {
+      gameElem.rollBtn.disabled = false;
+    }, 3500);
 
-		updateRollsLeft();
-
-		playerTotal = 0;
-		for (let i = 0; i < playerDiceValues.length; i++) {
-			playerTotal += playerDiceValues[i];
-		}
-		setTimeout(() => {
-			pElemTwo.innerHTML = `Player's roll: <b style="color: darkred">${playerTotal}</b>`;
-		}, 3500 / settings.animationSpeed);
-	}
+    updateRollsLeft(gameElem);
+    playerTotal = 0;
+    for (let i = 0; i < playerDiceValues.length; i++) {
+      playerTotal += playerDiceValues[i];
+    }
+    setTimeout(() => {
+      gameElem.pElemTwo.innerHTML = `Player's roll: <b style="color: darkred">${playerTotal}</b>`;
+    }, 3500);
+  }
 }
 
-function checkWinner() {
-	if (playerTotal > computerTotal) {
-		setTimeout(() => {
-			rollBtn.hidden = true;
-			h1Elem.hidden = true;
-			pElemTwo.innerHTML = `
+function checkWinner(gameElem) {
+  if (playerTotal > computerTotal) {
+    setTimeout(() => {
+      gameElem.rollBtn.hidden = true;
+      gameElem.h3Elem.hidden = true;
+      gameElem.pElemTwo.innerHTML = `
         Player's roll: <b style="color: #1d8602">${playerTotal}</b>
       `;
-			endScreenMsg.innerHTML = `
+      gameElem.endScreenMsg.innerHTML = `
         Congratulations I guess... You've beaten me with your lucky a** roll
          <b style="font-size: 26px; color: #1d8602;">${playerTotal}. 
         </b><br>Promise is a promise. Here's your grandpa's (insert item name)</br>
       `;
-			endScreenMsg.classList.remove("hidden");
-		}, 3500 / settings.animationSpeed);
-	} else if (attempts === 0) {
-		setTimeout(() => {
-			rollBtn.hidden = true;
-			h1Elem.style.color = "red";
-			endScreenMsg.textContent = `HAA NO ONE CAN BEAT AHMED! Show me the moneyyy... That is, if you want
+      gameElem.endScreenMsg.classList.remove('hidden');
+    }, 3500);
+  } else if (attempts === 0) {
+    setTimeout(() => {
+      gameElem.rollBtn.hidden = true;
+      gameElem.endScreenMsg.textContent = `HAA NO ONE CAN BEAT AHMED! Show me the moneyyy... That is, if you want
        your grandpa's (insert item name), because it's gonna cost ya.`;
-			endScreenMsg.classList.remove("hidden");
-		}, 3500 / settings.animationSpeed);
-	}
+      gameElem.endScreenMsg.classList.remove('hidden');
+    }, 3500);
+  }
 }
 
 function displayDiceValues(dice, values) {
-	dice.style.animation = `rolling ${3 / settings.animationSpeed}s`;
-	setTimeout(() => {
-		values.forEach((value) => {
-			const rotation = getRotationForValue(value);
+  dice.style.animation = 'rolling 3s';
+  setTimeout(() => {
+    values.forEach((value) => {
+      const rotation = getRotationForValue(value);
 
-			dice.style.transition = "0.2s";
-			dice.style.animationFillMode = "forwards";
-			dice.style.transform = rotation;
-			setTimeout(() => {
-				dice.style.transition = "1s ease";
-			}, 200 / settings.animationSpeed);
-		});
+      dice.style.transition = '0.2s';
+      dice.style.animationFillMode = 'forwards';
+      dice.style.transform = rotation;
+      setTimeout(() => {
+        dice.style.transition = '1s ease';
+      }, 200);
+    });
 
-		dice.style.animation = "none";
-	}, 3050 / settings.animationSpeed);
+    dice.style.animation = 'none';
+  }, 3050);
 }
 
 function getRotationForValue(value) {
-	switch (value) {
-		case 1:
-			return "rotateX(0deg) rotateY(0deg)";
-		case 6:
-			return "rotateX(180deg) rotateY(0deg)";
-		case 2:
-			return "rotateX(-90deg) rotateY(0deg)";
-		case 5:
-			return "rotateX(90deg) rotateY(0deg)";
-		case 3:
-			return "rotateX(0deg) rotateY(90deg)";
-		case 4:
-			return "rotateX(0deg) rotateY(-90deg)";
-		default:
-			return "rotateX(0deg) rotateY(0deg)";
-	}
+  switch (value) {
+    case 1:
+      return 'rotateX(0deg) rotateY(0deg)';
+    case 6:
+      return 'rotateX(180deg) rotateY(0deg)';
+    case 2:
+      return 'rotateX(-90deg) rotateY(0deg)';
+    case 5:
+      return 'rotateX(90deg) rotateY(0deg)';
+    case 3:
+      return 'rotateX(0deg) rotateY(90deg)';
+    case 4:
+      return 'rotateX(0deg) rotateY(-90deg)';
+    default:
+      return 'rotateX(0deg) rotateY(0deg)';
+  }
 }
 
-window.addEventListener("DOMContentLoaded", rollComputerDices);
-rollBtn.addEventListener("click", () => {
-	rollPlayerDices();
-	checkWinner();
+window.addEventListener('DOMContentLoaded', () => rollComputerDices(gameElem));
+gameElem.rollBtn.addEventListener('click', () => {
+  rollPlayerDices(gameElem);
+  checkWinner(gameElem);
 });
